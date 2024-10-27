c25ea05b34f812e740539cf9e35e87fea81392a7
os-till-go <128401541+os-till-go@users.noreply.github.com>
2024-10-27 21:48:05 +0800

    test

---
+++ MR-LCS.cpp
@@ -0,0 +1,6 @@
+#include "Circuit.h"
+#include "ALAP.h"
+#include "ASAP.h"
+#include "MR-LCS.h"
+
+/**/
---
+++ MR-LCS.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#ifndef MR_LCS_H
+#define MR_LCS_H
+
+#include "Circuit.h"
+#include <algorithm>
+
+class MR_LCS : public Scheduler {
+
+};
+
+#endif // !MR_LCS_H
