e95c81e9e509e90aa9a6c27fedbfa0ef4636de5d
os-till-go <128401541+os-till-go@users.noreply.github.com>
2024-10-31 22:55:22 +0800

    还原3

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include "ALAP.h"
 #include "ASAP.h"
 #include "MR-LCS.h"
+#include"ALAP_Latency.h"
+#include"ASAP_Latency.h"
 
 
 int main() {
@@ -18,6 +20,45 @@ int main() {
 
     std::cout << "blif to verilog 转换完成" << std::endl;
 
+    ALAP_L al;
+
+    al.ALAP_Lschedule(verilog);
+    verilog.printSchedule(verilog, al);
+
+    //TEST sample02
+    Circuit verilog02;
+    std::string blifFile02 = "sample04.blif";//TODO
+    std::string verilogFile02 = "sample04.v";
+
+    parseBLIF(blifFile02, verilog02);
+    writeVerilog(verilogFile02, verilog02);
+
+    std::cout << "转换完成02" << std::endl;
+
+    /*ALAP al02;
+    al02.schedule(verilog02);
+    al02.ALAPschedule(verilog02);
+    verilog02.printSchedule(verilog02, al02);*/
+    /*
+//    Total 6 Cycles
+//Cycle 0: {g}, {f}, {},
+//Cycle 1: {}, {j}, {h},
+//Cycle 2: {}, {i}, {},
+//Cycle 3: {k}, {}, {},
+//Cycle 4: {}, {}, {l},
+//Cycle 5: {}, {}, {o p},
+//    
+//    */
+    ALAP_L alap_l;
+    alap_l.ALAP_Lschedule(verilog02);
+    verilog02.printSchedule(verilog02, alap_l);
+
+
+   /* ASAPScheduler asapScheduler;
+    asapScheduler.schedule(verilog02);
+    verilog.printSchedule(verilog02, asapScheduler);*/
+
+  
     //TEST ASAP
     //ASAPScheduler asapScheduler;
     //asapScheduler.schedule(verilog);
