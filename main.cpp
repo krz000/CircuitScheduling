43fa823a50c637a59eea55f7f1b7d943d0ae8a46
os-till-go <128401541+os-till-go@users.noreply.github.com>
2024-10-31 22:42:46 +0800

    还原2

--- main.cpp
+++ main.cpp
@@ -3,18 +3,22 @@
 #include "ASAP.h"
 #include"ALAP_Latency.h"
 #include"ASAP_Latency.h"
+#include "MR-LCS.h"
+
 
 int main() {
-
-    //TEST sample1
     Circuit verilog;
-    std::string blifFile = "test1.blif";//TODO
+	//std::cout << "请输入BLIF文件名" << std::endl;
+    //std::string blifFile;
+    //std::cin >> blifFile;
+	//std::string verilogFile = blifFile + ".v";
+    std::string blifFile = "03.txt";//TODO
     std::string verilogFile = "test1.v";
     
     parseBLIF(blifFile, verilog);
     writeVerilog(verilogFile, verilog);
 
-    std::cout << "转换完成" << std::endl;
+    std::cout << "blif to verilog 转换完成" << std::endl;
 
     ALAP_L al;
 
@@ -55,16 +59,16 @@ int main() {
     verilog.printSchedule(verilog02, asapScheduler);*/
 
   
-	//TEST ALAP
-    
-   /* ALAP ALAPscheduler;
-	ALAPscheduler.schedule(verilog02);
-    ALAPscheduler.ALAPschedule(verilog02);
-    verilog.printSchedule(verilog02, ALAPscheduler);*/
+    //TEST ASAP
+    //ASAPScheduler asapScheduler;
+    //asapScheduler.schedule(verilog);
+    //verilog.printSchedule(verilog, asapScheduler);
 
-    
-   
-    
+	//TEST ALAP
+    ALAP ALAPscheduler;
+	ALAPscheduler.schedule(verilog);
+    ALAPscheduler.ALAPschedule(verilog);
+    verilog.printSchedule(verilog, ALAPscheduler);
     
     /* ALAP答案
     Input :a ,b ,c ,d ,e ,f   Output :o ,p ,q
@@ -74,5 +78,21 @@ int main() {
     Cycle 2: {n}, {k}, {},
     Cycle 3: {o}, {}, {p q},
     */
+
+    //TEST MR_RCS
+	// 输入资源得结果
+
+
+    // TEST MR_LCS
+    // 输入目的 cycle
+ //   int targetCycle;
+	//std::cout << "请输入目的cycle" << std::endl;
+ //   std::cin >> targetCycle;
+ //   MR_LCS mr_lcsScheduler;
+	//mr_lcsScheduler.schedule(verilog);
+	//mr_lcsScheduler.MR_LCSscheduleBF(verilog, targetCycle);
+	//verilog.printSchedule(verilog, mr_lcsScheduler);
+	//std::cout << "MR_LCS 对于 cycle = " + targetCycle << "所需资源数: " + mr_lcsScheduler.getResourseNum() << std::endl;
+
     return 0;
 }
